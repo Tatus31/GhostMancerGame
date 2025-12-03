37ac4204fa4eb422f991fa3b7b971d3e429f64e4
Tatus31 <kris.zborowski2003@gmail.com>
2025-12-03 13:51:42 +0100

    some basic refactoring

--- Assets/Scripts/PlayerMovement/Player.cs
+++ Assets/Scripts/PlayerMovement/Player.cs
@@ -6,20 +6,25 @@ namespace PlayerMovement
     [RequireComponent( typeof(PlayerController), typeof(PlayerInput) )]
     public class Player : MonoBehaviour
     {
-        [Header("Jump")]
+        [Header("[Jump]")]
         [Range(0f, 100f)]
         [SerializeField] private float maxJumpHeight = 4f;
         [Range(0f, 100f)]
         [SerializeField] private float minJumpHeight = 1f;
         [Range(0f, 10f)]
         [SerializeField] private float timeToJumpApex = 0.4f;
-        [Header("Movement")]
+        [Header("[Movement]")]
         [Range(0f, 100f)]
         [SerializeField] private float moveSpeed = 5f;
+        [Header("[Acceleration/Deceleration]")]
         [Range(0f, 100f)]
         [SerializeField] private float accelerationOnGround = 0.1f;
         [Range(0f, 100f)]
         [SerializeField] private float accelerationInAir = 0.2f;
+        [Range(0f, 100f)]
+        [SerializeField] private float decelerationOnGround = 0.1f;
+        [Range(0f, 100f)]
+        [SerializeField] private float decelerationInAir = 0.2f;
 
         private float _gravity;
         private float _maxJumpVelocity;
@@ -38,10 +43,8 @@ namespace PlayerMovement
         {
             _playerController = GetComponent<PlayerController>();
             _playerInput =  GetComponent<PlayerInput>();
-            
-            _gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-            _maxJumpVelocity = Mathf.Abs(_gravity) * timeToJumpApex;
-            _minJumpVelocity = Mathf.Sqrt(2 *  Mathf.Abs(_gravity) * minJumpHeight);
+
+            JumpVariableSetup();
         }
 
         private void Update()
@@ -63,16 +66,31 @@ namespace PlayerMovement
         {
             Vector2 input = _playerInput.MoveInput;
 
-            if (_playerController.GetCollisionInfo.Bottom ||  _playerController.GetCollisionInfo.Top)
+            //No idea how to call that
+            if (_playerController.GetCollisionInfo.Bottom || _playerController.GetCollisionInfo.Top)
             {
                 _velocity.y = 0;
             }
+
+            OnMaxJump();
+            OnVariableJump();
             
+            _wasJumpPressed = false;
+            _wasJumpReleased  = false;
+
+            OnPlayerMovement(input);
+        }
+
+        private void OnMaxJump()
+        {
             if(_wasJumpPressed && !_wasJumpReleased && _playerController.GetCollisionInfo.Bottom)
             {
                 _velocity.y = _maxJumpVelocity;
             }
+        }
 
+        private void OnVariableJump()
+        {
             if (_wasJumpReleased && !_wasJumpPressed)
             {
                 if (_velocity.y > _minJumpVelocity)
@@ -80,15 +98,34 @@ namespace PlayerMovement
                     _velocity.y = _minJumpVelocity;
                 }
             }
-            
-            _wasJumpPressed = false;
-            _wasJumpReleased  = false;
-            
+        }
+
+        private void OnPlayerMovement(Vector2 input)
+        {
             float targetVelocityX = input.x * moveSpeed;
-            _velocity.x = Mathf.SmoothDamp(_velocity.x, targetVelocityX, ref _velocityXSmoothing, _playerController.GetCollisionInfo.Bottom ? accelerationOnGround : accelerationInAir);
+            float accelerateOrDecelerate;
+
+            if (_playerController.GetCollisionInfo.Bottom)
+            {
+                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? accelerationOnGround : decelerationOnGround;
+                _velocity.x = Mathf.SmoothDamp(_velocity.x, targetVelocityX, ref _velocityXSmoothing, accelerateOrDecelerate);
+            }
+            else
+            {
+                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? accelerationInAir : decelerationInAir;
+                _velocity.x = Mathf.SmoothDamp(_velocity.x, targetVelocityX, ref _velocityXSmoothing, accelerateOrDecelerate);
+            }
+
             _velocity.y += _gravity * Time.fixedDeltaTime; 
             
             _playerController.Displacement(_velocity * Time.fixedDeltaTime);
+        }
+
+        private void JumpVariableSetup()
+        {
+            _gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+            _maxJumpVelocity = Mathf.Abs(_gravity) * timeToJumpApex;
+            _minJumpVelocity = Mathf.Sqrt(2 *  Mathf.Abs(_gravity) * minJumpHeight);
         }
     }
 }
