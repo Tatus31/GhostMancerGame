551090c3e5a3eae11d9a2d68d16e3c0c46051380
Tatus31 <kris.zborowski2003@gmail.com>
2025-12-09 15:55:46 +0100

    added jump buffering and tooltips to Player SO

--- Assets/Scripts/PlayerMovement/Player.cs
+++ Assets/Scripts/PlayerMovement/Player.cs
@@ -1,35 +1,39 @@
 using System;
+using System.Collections;
+using System.Threading.Tasks;
 using PlayerMovement.PlayerData;
 using UnityEngine;
 
 namespace PlayerMovement
 {
-    [RequireComponent( typeof(PlayerController), typeof(PlayerInput) )]
+    [RequireComponent(typeof(PlayerController), typeof(PlayerInput))]
     public class Player : MonoBehaviour
     {
         public event Action<float> OnGravityChanged;
-        
-       [SerializeField] private PlayerDataSO _playerData;
+
+        [SerializeField] private PlayerDataSO playerData;
 
         private float _gravity;
         private float _maxJumpVelocity;
         private float _minJumpVelocity;
         private float _velocityXSmoothing;
-        
+        private float _jumpBufferCurrentTime;
+
         private Vector2 _velocity;
-        
+
         private bool _wasJumpPressed;
         private bool _wasJumpReleased;
-        
+        private bool _startBufferTimer;
+
         private PlayerController _playerController;
         private PlayerInput _playerInput;
-        
+
         public Vector2 Velocity => _velocity;
 
         void Start()
         {
             _playerController = GetComponent<PlayerController>();
-            _playerInput =  GetComponent<PlayerInput>();
+            _playerInput = GetComponent<PlayerInput>();
 
             JumpVariableSetup();
         }
@@ -38,14 +42,24 @@ namespace PlayerMovement
         {
             if (_playerInput.WasJumpPressed)
             {
-                _wasJumpPressed =  true;
+                // if(_playerInput.WasJumpPressed && !_playerController.GetCollisionInfo.Bottom)
+                //
+                _startBufferTimer = true;
+                _jumpBufferCurrentTime = 0f;
+
+                _wasJumpPressed = true;
                 _playerInput.OnPlayerConsumeJump();
             }
 
             if (_playerInput.WasJumpReleased)
             {
-                _wasJumpReleased  = true;
+                _wasJumpReleased = true;
                 _playerInput.OnPlayerConsumeJumpRelease();
+            }
+
+            if (_startBufferTimer)
+            {
+                _jumpBufferCurrentTime += Time.deltaTime;
             }
         }
 
@@ -53,27 +67,45 @@ namespace PlayerMovement
         {
             Vector2 input = _playerInput.MoveInput;
 
-            //No idea how to call that
+            // Player landed on ground
             if (_playerController.GetCollisionInfo.Bottom || _playerController.GetCollisionInfo.Top)
             {
                 _velocity.y = 0;
+                
+                //check if there is a jump buffered
+                if (_startBufferTimer && _jumpBufferCurrentTime <= playerData.jumpBufferMaxTime)
+                {                    
+                    //Debug.Log($"player clicked space {jumpBufferCurrentTime} seconds before landing");
+                    
+                    OnMaxJumpAfterBuffer();
+                    OnVariableJump();
+                    _wasJumpPressed = false;
+                }
+
+                _startBufferTimer = false;
+                _jumpBufferCurrentTime = 0f;
             }
 
             OnMaxJump();
             OnVariableJump();
-            
+
             _wasJumpPressed = false;
-            _wasJumpReleased  = false;
+            _wasJumpReleased = false;
 
             OnPlayerMovement(input);
         }
 
         private void OnMaxJump()
         {
-            if(_wasJumpPressed && !_wasJumpReleased && _playerController.GetCollisionInfo.Bottom)
+            if (_wasJumpPressed && !_wasJumpReleased && _playerController.GetCollisionInfo.Bottom)
             {
                 _velocity.y = _maxJumpVelocity;
             }
+        }
+
+        private void OnMaxJumpAfterBuffer()
+        {
+            _velocity.y = _maxJumpVelocity;
         }
 
         private void OnVariableJump()
@@ -87,15 +119,23 @@ namespace PlayerMovement
             }
         }
 
+        // private IEnumerator JumpBufferCoroutine()
+        // {
+        //     while (!_playerController.GetCollisionInfo.Bottom && )
+        //     {
+        //         
+        //     }
+        // }
+
         private void OnPlayerMovement(Vector2 input)
         {            
-            float targetVelocityX = input.x * _playerData.moveSpeed;
+            float targetVelocityX = input.x * playerData.moveSpeed;
             float atApexPosition = 0;
             
             if (!_playerController.GetCollisionInfo.Bottom)
             {
-                atApexPosition = Mathf.InverseLerp(0, _playerData.apexThreshold, Mathf.Abs(_velocity.y));
-                float atApexVelocityBonus = 1f + (atApexPosition * _playerData.atApexSpeedBonus);
+                atApexPosition = Mathf.InverseLerp(0, playerData.apexThreshold, Mathf.Abs(_velocity.y));
+                float atApexVelocityBonus = 1f + (atApexPosition * playerData.atApexSpeedBonus);
                 targetVelocityX *= atApexVelocityBonus;
             }
             
@@ -103,25 +143,25 @@ namespace PlayerMovement
 
             if (_playerController.GetCollisionInfo.Bottom)
             {
-                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? _playerData.accelerationOnGround : _playerData.decelerationOnGround;
+                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? playerData.accelerationOnGround : playerData.decelerationOnGround;
                 _velocity.x = Mathf.SmoothDamp(_velocity.x, targetVelocityX, ref _velocityXSmoothing, accelerateOrDecelerate);
             }
             else
             {
-                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? _playerData.accelerationInAir : _playerData.decelerationInAir;
+                accelerateOrDecelerate = (Mathf.Abs(input.x) > 0.01f) ? playerData.accelerationInAir : playerData.decelerationInAir;
                 _velocity.x = Mathf.SmoothDamp(_velocity.x, targetVelocityX, ref _velocityXSmoothing, accelerateOrDecelerate);
             }
 
             float currentGravity = _gravity;
             if (!_playerController.GetCollisionInfo.Bottom)
             {
-                currentGravity = Mathf.Lerp(_gravity * _playerData.apexGravityMultiplier, _gravity, atApexPosition);
+                currentGravity = Mathf.Lerp(_gravity * playerData.apexGravityMultiplier, _gravity, atApexPosition);
             }
 
             OnGravityChanged?.Invoke(currentGravity);
 
             _velocity.y += currentGravity * Time.fixedDeltaTime;
-            _velocity.y = Mathf.Clamp(_velocity.y,-_playerData.maxFallSpeed, _playerData.maxFallSpeed );
+            _velocity.y = Mathf.Clamp(_velocity.y,-playerData.maxFallSpeed, playerData.maxFallSpeed );
             
             // if(_velocity.y <= maxFallSpeed)
             //     _velocity.y += currentGravity * Time.fixedDeltaTime; 
@@ -133,9 +173,9 @@ namespace PlayerMovement
 
         private void JumpVariableSetup()
         {
-            _gravity = -(2 * _playerData.maxJumpHeight) / Mathf.Pow(_playerData.timeToJumpApex, 2);
-            _maxJumpVelocity = Mathf.Abs(_gravity) * _playerData.timeToJumpApex;
-            _minJumpVelocity = Mathf.Sqrt(2 *  Mathf.Abs(_gravity) * _playerData.minJumpHeight);
+            _gravity = -(2 * playerData.maxJumpHeight) / Mathf.Pow(playerData.timeToJumpApex, 2);
+            _maxJumpVelocity = Mathf.Abs(_gravity) * playerData.timeToJumpApex;
+            _minJumpVelocity = Mathf.Sqrt(2 *  Mathf.Abs(_gravity) * playerData.minJumpHeight);
         }
 
         private void OnValidate()
--- Assets/Scripts/PlayerMovement/PlayerData/PlayerDataSO.cs
+++ Assets/Scripts/PlayerMovement/PlayerData/PlayerDataSO.cs
@@ -6,19 +6,30 @@ namespace PlayerMovement.PlayerData
     public class PlayerDataSO : ScriptableObject
     {
         [Header("[Jump]")]
+        [Tooltip("How high the player can jump")]
         [Range(0f, 100f)]
         public float maxJumpHeight = 4f;
+        [Tooltip("How small can the player jump height be")]
         [Range(0f, 100f)]
         public float minJumpHeight = 1f;
+        [Tooltip("How long it takes for the player to reach the apex of his jump")]
         [Range(0f, 10f)]
         public float timeToJumpApex = 0.4f;
+        [Tooltip("Determines when player is considered to have reached the apex of his jump")]
         [Range(0f, 100f)]
         public float apexThreshold = 1f;
+        [Tooltip("How much the gravity affecting the player will change at the apex")]
         [Range(0f, 10f)]
         public float apexGravityMultiplier = 0.4f;
+        [Tooltip("For how long will a jump buffer even if the player clicks the jump button without touching the ground")]
+        [Range(0f, 1f)]
+        public float jumpBufferMaxTime = 0.1f;
+
         [Header("[Movement]")]
+        [Tooltip("The maximum speed the player can achieve")]
         [Range(0f, 100f)]
         public float moveSpeed = 5f;
+        [Tooltip("How much faster the player becomes at the apex")]
         [Range(0f, 1f)]
         public float atApexSpeedBonus = 0.2f;
         [Header("[Acceleration/Deceleration]")]
@@ -30,7 +41,9 @@ namespace PlayerMovement.PlayerData
         public float decelerationOnGround = 0.1f;
         [Range(0f, 100f)]
         public float decelerationInAir = 0.2f;
+
         [Header("[Movement Limits]")]
+        [Tooltip("How fast can the player fall")]
         [Range(0f, 100f)]
         public float maxFallSpeed = 10f;
     }
